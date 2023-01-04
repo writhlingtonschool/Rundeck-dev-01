dcb91dc3dedf3c419506c0cf6f9a9ce059907d4a
writhlingtonschool-snoble <snoble@writhlington.org.uk>
2023-01-04 16:39:21 +0000

    Update dev001.ps1

--- dev001.ps1
+++ dev001.ps1
@@ -1,17 +1,5 @@
-$CID="C@option.ChangeID@"
-$GamDir="D:\AppData\GAMXTD3\app"
-$DataDir="D:\AppData\Rundeck\$CID\Data"
-$LogDir="D:\AppData\Rundeck\$CID\Logs"
-$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
-$tempcsv = "$DataDir\temp.csv"
-$temptxt = "$DataDir\temp.txt"
 
-Write-Host $(Get-Date)
-
-$ErrorActionPreference="Continue"
 
 Write-Host "Hello..."
 Write-Host "email from rundeck var:" $email
 Write-Host "Api key from rundeck var" $GLPIapiAppToken
-
-#Stop-Transcript
