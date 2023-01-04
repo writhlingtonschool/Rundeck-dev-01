69f38e7f8943fb039932cc98ac9af6e549b1fd80
writhlingtonschool-snoble <snoble@writhlington.org.uk>
2023-01-04 16:19:00 +0000

    Update dev001.ps1

--- dev001.ps1
+++ dev001.ps1
@@ -6,13 +6,9 @@ $transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
 $tempcsv = "$DataDir\temp.csv"
 $temptxt = "$DataDir\temp.txt"
 
-Start-Transcript -Path $transcriptlog -Force -NoClobber -Append
 Write-Host $(Get-Date)
-
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/writhlingtonschool/Rundeck-dev-01/main/dev001.ps1' -OutFile $DataDir\test1.ps1
-
 
 $ErrorActionPreference="Continue"
 
 
-Stop-Transcript
+#Stop-Transcript
